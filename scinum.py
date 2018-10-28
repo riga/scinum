ea51a9dd67df7d5b431f3bca462a57008ed9b46b
M. Rieger <marcelrieger@me.com>
2018-10-28 15:13:03 +0100

    Version 0.2.6.

--- scinum.py
+++ scinum.py
@@ -12,7 +12,7 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/scinum"
 __license__ = "MIT"
 __status__ = "Development"
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __all__ = ["Number", "Operation", "ops", "REL", "ABS", "NOMINAL", "UP", "DOWN"]
 
 
