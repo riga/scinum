284ec32c5bbac1f88e2cadbc16be00614832ad9b
M. Rieger <marcelrieger@me.com>
2020-04-08 17:19:00 +0200

    Version 1.1.1.

--- scinum.py
+++ scinum.py
@@ -13,7 +13,7 @@ __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/scinum"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __all__ = ["Number", "Operation", "ops", "style_dict", "REL", "ABS", "NOMINAL", "UP", "DOWN"]
 
 
