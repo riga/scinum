37c510e32b144e72cd52e6fd944b1c2c4c63e296
Marcel Rieger <riga@users.noreply.github.com>
2025-01-04 19:58:12 +0100

    License.

--- scinum/__init__.py
+++ scinum/__init__.py
@@ -8,7 +8,7 @@ support.
 from __future__ import annotations
 
 __author__ = "Marcel Rieger"
-__copyright__ = "Copyright 2017-2024, Marcel Rieger"
+__copyright__ = "Copyright 2017-2025, Marcel Rieger"
 __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/scinum"
 __license__ = "BSD-3-Clause"
